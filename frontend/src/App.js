211fc9290b622c69adbf27e9bfbbf8130e088ef5
Gabriel Veloz <gabriel.veloz@fluke.com>
2025-07-26 07:52:46 -0700

    make BlogApp the entrypoint for root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BlogApp from './BlogApp'; // this is the renamed MUI Blog App
-import Home from './Home'; // or whatever your main page is
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BlogApp from "./BlogApp"; // this is the renamed MUI Blog App
+// import Home from "./Home"; // or whatever your main page is
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<BlogApp />} />
+        <Route path="/" element={<BlogApp />} />
       </Routes>
     </Router>
   );
